ba8ccc8db3e1fe357987e6aefa82c681206ef4d3
tiredant <zulufoxtrot1@live.com>
2015-05-26 07:58:05 -0700

    Update fbxy.py

--- fbxy.py
+++ fbxy.py
@@ -44,3 +44,43 @@ for each in current:
 			how_high = each[4:10]
 			bench.append(int(how_high))
 			
+			print \
+			
+			
+			for i in range(1,int(bench[2])+1),:
+				for z in i:
+					if z%bench[0]==0 and z%bench[1]==0:
+						print 'FB',
+					elif z%bench[0]==0:
+						print 'F',
+					elif z%bench[1]==0:
+						print 'B',
+					elif z > int(bench[2]):
+						print z,
+					else:
+						print z,
+				
+	else:
+		fizz = each[0:1]
+		bench.append(int(fizz))
+		buzz = each[2:4]
+		bench.append(int(buzz))
+		how_high = each[4:10]
+		bench.append(how_high)
+		
+		print \
+		
+		
+		for i in range(1,int(bench[2])+1),:
+			for z in i:
+				if z%bench[0]==0 and z%bench[1]==0:
+					print 'FB',
+				elif z%bench[0]==0:
+					print 'F',
+				elif z%bench[1]==0:
+					print 'B',
+				elif z > int(bench[2]):
+					print z,
+				else:
+					print z,
+		
